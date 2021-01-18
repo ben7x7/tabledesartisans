8dbd987e2a1869cddfe4d25fd7da189befe56e36
benoit bessiere <benoit.bessiere@gmail.com>
2021-01-18 11:14:06 +0100

    set i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  get 'cookies',          to: 'pages#cookies',            as: :cookies
-  get 'mentions',         to: 'pages#mentions',           as: :mentions
-  get 'confidentialite',  to: 'pages#confidentialite',    as: :confidentialite
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    get 'cookies',          to: 'pages#cookies',            as: :cookies
+    get 'mentions',         to: 'pages#mentions',           as: :mentions
+    get 'confidentialite',  to: 'pages#confidentialite',    as: :confidentialite
+  end
 end
