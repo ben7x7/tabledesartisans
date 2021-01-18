531110acba3ca580fce9997992a728effa6fd7ce
benoit bessiere <benoit.bessiere@gmail.com>
2021-01-18 10:12:01 +0100

    add mentions legales

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'cookies',          to: 'pages#cookies',            as: :cookies
+  get 'mentions',         to: 'pages#mentions',           as: :mentions
+  get 'confidentialite',  to: 'pages#confidentialite',    as: :confidentialite
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
